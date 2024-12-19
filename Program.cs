69bd48d3d8f7601335e0303d4317d6be00bb2510
Hesham Mathana <heshammathana0@gmail.com>
2024-12-19 23:53:41 +0200

    this app print Hello and the name enterd by user

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hello__name_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter name");
+            string name=Console.ReadLine();
+            Console.WriteLine($"Hello,{name}");
+        }
+    }
+}
